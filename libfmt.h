43eb67a6571f8d1239d3f48fae0e1f6ec553de83
Quentin Le Guennec <quentin.leguennec1@gmail.com>
2017-01-09 15:27:08 +0100

    fix norm

--- libfmt.h
+++ libfmt.h
@@ -6,7 +6,7 @@
 /*   By: qle-guen <qle-guen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/23 02:52:19 by qle-guen          #+#    #+#             */
-/*   Updated: 2017/01/06 16:27:52 by qle-guen         ###   ########.fr       */
+/*   Updated: 2017/01/09 15:26:11 by qle-guen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,9 @@
 
 int			fmt_print(int fd, int ret, char *s, size_t n, ...);
 void		fmt_fmt(t_vect *a, char *s, size_t n, va_list l);
+void		fmt_int(t_vect *a, char **d, size_t *n, va_list l);
+void		fmt_int_unsign(t_vect *a, t_u32 base, t_u64 x);
+void		fmt_int_sign_32(t_vect *a, t_i32 base, t_i32 x);
+void		fmt_int_sign_64(t_vect *a, t_i32 base, t_i64 x);
 
 #endif
